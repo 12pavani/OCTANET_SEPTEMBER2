0dc4da3ade81ecbf5d853e6de03efafc4613957e
Pavani <125529799+12pavani@users.noreply.github.com>
2023-09-18 19:44:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Get the selected priority
+    const prioritySelect = document.querySelector(".priority-select");
+    const priority = prioritySelect.value;
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value + " (Priority: " + priority + ")";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //ADDING TO LOCAL STORAGE 
+    saveLocalTodos(todoInput.value + " (Priority: " + priority + ")");
+    
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    if(item.classList.contains("trash-btn")) {
+        const todo = item.parentElement;
+        todo.classList.add("slide");
+
+        removeLocalTodos(todo);
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        });
+    }
+
+    if(item.classList.contains("complete-btn")) {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all": 
+                todo.style.display = "flex";
+                break;
+            case "completed": 
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "incomplete":
+                if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getLocalTodos() {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function(todoWithPriority) {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+
+        // Split the saved to-do item to get the text and priority
+        const [todoText, priority] = todoWithPriority.split(" (Priority: ");
+
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoText;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+
+        // Create and set the priority select
+        const prioritySelect = document.createElement("select");
+        prioritySelect.classList.add("priority-select");
+        prioritySelect.innerHTML = `
+            <option value="low" ${priority === 'Low)' ? 'selected' : ''}>Low</option>
+            <option value="medium" ${priority === 'Medium)' ? 'selected' : ''}>Medium</option>
+            <option value="high" ${priority === 'High)' ? 'selected' : ''}>High</option>
+        `;
+        todoDiv.appendChild(prioritySelect);
+
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    const todoIndex = todo.querySelector(".todo-item").innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
